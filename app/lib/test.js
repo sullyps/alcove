c99cf11cf0181e0868c837dd1aa5a2dcf444c1f1
Michael Lyon <malyon@bioneos.com>
2016-01-20 10:29:28 -0600

    Fixed next scheduled backup time code.

--- app/lib/test.js
+++ app/lib/test.js
@@ -1,14 +1,14 @@
 var buckets = require('./buckets.js');
 
-var schedule = '0,1,2,3,4,5,6(7)|1(5)_[3:01]';
-var date = new Date('2016-14-Jan_09:54');
-var bucketsVar = buckets.getBuckets(schedule, date);
-var dir = '../../resources/test_backups_1/';
+var schedule = '0,2(6)|1(5)_[3:01]';
+var dir = '../../resources/test_backups_2/';
+var date = new Date('2016-02-Jan-09:54');
 
+var bucketsVar = buckets.getBuckets(schedule, date);
 console.log('Buckets:');
 bucketsVar.forEach(function(bucket) {
   console.log('  ' + bucket.date);
 });
-buckets.fillBuckets(bucketsVar, dir, buckets.removeDirectories);
-
-console.log('\nNext scheduled time: ' + buckets.findNextScheduledTime(schedule, new Date()));
+//buckets.fillBuckets(bucketsVar, dir, buckets.removeDirectories);
+buckets.fillBuckets(bucketsVar, dir, buckets.listDirectoriesToRemove);
+console.log('\nNext scheduled time: ' + buckets.findNextScheduledTime(schedule, date));
