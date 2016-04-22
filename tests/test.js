20ca7151db7f961db4f017f8a59576c88743e108
Michael Lyon <malyon@bioneos.com>
2016-04-22 14:30:40 -0500

    Addition of tape to packages.json

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,30 @@
+var test = require('tape');
 var system = require('./system');
 
 var schedule = '0,1,2,3,4,5,6(7)|1(5);9:15';
+
+// Tests for parsing the schedule string into an object.
+test('schedule parsing', function(assert) {
+  assert.plan(1);
+  
+});
+
+// Testing bucket generation from a schedule object
+
+
+//Testing filling buckets given a directory structure and buckets.
+
+
+// Testing finding the next expected backup date given date and schedule.
+
+
+
+// Identify which backups to remove given schedule and directory
+
+
+
+
+
 // var dir = '../../resources/test_backups_2/';
 var dir = '~/backupTest/beta/';
 var date = new Date('2016-02-Jan-03:02');
