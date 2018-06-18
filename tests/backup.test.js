0466b07da8b145acd40899280a0854a307a65827
Olivia Sandvold <ollie302@gmail.com>
2018-06-18 11:07:39 -0500

    Removed console.log checkpoints in before/aftereach
    * These were used to determine the order in which the statements would
    be executed

--- tests/backup.test.js
+++ tests/backup.test.js
@@ -14,17 +14,13 @@ var invalidNames = ['incorrect-dir-name','2018-06-15','2018-06-15T18:49:00:000']
 beforeEach(function() {
   if (fs.existsSync(dir))
     fs.removeSync(dir);
-  console.log('Making test directory');
   fs.mkdirSync(dir);
 });
 
 // Always remove the directory with the simulated data after each test
 afterEach(function() {
   if (fs.existsSync(dir))
-  {
     fs.removeSync(dir);
-    console.log('Removing test directory');
-  }
 });
 
 describe('Only valid directories inside backup directory', function() {
@@ -53,7 +49,6 @@ describe('Valid file names but no directories', function() {
 
 describe('Files and directories with valid names', function() {
   beforeEach(function() {
-    console.log('Defining const file and directory date arrays');
     const fileDates = ['2018-06-15T08:00:00.000Z','2018-06-13T08:00:00.000Z','2018-06-11T08:00:00.000Z','2018-06-09T08:00:00.000Z'];
     const dirDates = ['2018-06-15T07:59:59.000Z','2018-06-13T07:59:59.000Z','2018-06-11T07:59:59.000Z','2018-06-09T07:59:59.000Z'];
     fileDates.forEach(function(filename) {
