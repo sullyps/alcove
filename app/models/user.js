4a8dfd7696c3f573e92fdab5c41e3fd36a967d50
Olivia Sandvold <ollie302@gmail.com>
2018-06-28 16:18:46 -0500

    Need to determine how to label access for users

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, DataTypes) => {
   let User = sequelize.define('User', {
     'username' : DataTypes.STRING,
     'password' : DataTypes.STRING,
-    'access' : DataTypes.STRING
+    // TODO: Determine how we would like to define access levels
+    //'access' : DataTypes.STRING // DataTypes.INTEGER
   }, {
     classMethods: {
       associate: (models) => {
