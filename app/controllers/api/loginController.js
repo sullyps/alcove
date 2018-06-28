dd7ece9e04e0b2fbea2311db1fce11c086f2580a
Olivia Sandvold <ollie302@gmail.com>
2018-06-28 15:17:41 -0500

    Basic logic in place to check db for user
    * Need to add hashing of password before database query
    * Just requiring the models/ directory does not pull out the instance of
    the database

--- app/controllers/api/loginController.js
+++ app/controllers/api/loginController.js
@@ -8,12 +8,10 @@ const db = require('../../models');
 router.use(bodyParser.urlencoded({ extended : true }));
 router.use(bodyParser.json())
 
-// TODO: Will want to hash the password and query for that hash
 router.post('/', (req, res) => {
-  res.status(200).send('The object ' + req.name);
-  /*db.User.findOne({
+  db.User.findOne({
     where: {
-      // TODO: Determine what format this request will be in
+      // TODO: Will want to hash the password and query for that hash
       username : req.body.username,
       password : req.body.password
     }
@@ -24,7 +22,7 @@ router.post('/', (req, res) => {
   })
   .catch( err => {
     return res.status(401).send("There was a problem finding that user in the database");
-  });*/
+  });
 });
 
 module.exports = (app => {
