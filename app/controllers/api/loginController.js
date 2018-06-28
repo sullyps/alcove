0b2e9ed55dd8f9bfa7edf6ad469aab5c5c2eb26c
Olivia Sandvold <ollie302@gmail.com>
2018-06-28 11:19:53 -0500

    GET method to get a single user from the database

--- app/controllers/api/loginController.js
+++ app/controllers/api/loginController.js
@@ -10,6 +10,8 @@ router.post('/', (req, res) => {
   // TODO: Determine if I need the instance of the db to properly
   // utilize the .create() function.
   User.create({
+    // TODO: Determine if the request will have a body & name attributes,
+    // how does that work?
     'username' : req.body.name,
     // TODO: This password will need to be hashed and stored in db
     'password' : req.body.password,
@@ -19,9 +21,21 @@ router.post('/', (req, res) => {
     res.status(200).send(user);
   })
   .catch(err) {
-    return res.status(401).send("Failed to add information to database");
+    return res.status(401).send("There was a problem adding the user to the databse.");
   }
 });
+
+// Get a single user from the database
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id)
+  .then( user = > {
+    if (!user) return res.status(401).send("No user found");
+    res.status(200).send(user);
+  })
+  .catch(err) {
+    return res.status(401).send("There was a problem finding the user.")
+  }
+})
 
 // NOTE: I wrote this method thinking that there would be a need for it,
 // but I don't think we will want to allow seeing all of the users in the db.
@@ -31,6 +45,6 @@ router.get('/', (req, res) => {
     res.send(200).send(users);
   })
   .catch(err) {
-    return res.status(401).send("Failure in finding all users");
+    return res.status(401).send("There was a problem finding all users");
   }
 });*/
