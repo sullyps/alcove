8a394f525ab36a5c2d5651c08422592b6cb2e6b1
Olivia Sandvold <ollie302@gmail.com>
2018-06-29 11:36:52 -0500

    Renamed to eliminate redundancy of file naming

--- app/controllers/api/login.js
+++ app/controllers/api/login.js
@@ -2,30 +2,13 @@ const express = require('express'),
   router = express.Router(),
   bodyParser = require('body-parser');
 
-let db = {};
-
-try
-{
-  db = require('../../models').getDatabase();
-}
-catch (err)
-{
-  // TODO: Discuss Methods for handling an un-initialized database at this
-  // point of the program.
-  // Several possible outcomes:
-  // 1. Inside each route, check if db initialized, kill Node if not.
-  // 2. Inside function that returns database, kill Node if not initialized.
-  // 3. Set a flag and check flag for each post/get response (not ideal).
-  // Implementing option 1 until further discussion.
-  console.error('*** The database has NOT been initialized. Exiting program ***');
-  process.exit(-1);
-}
+let db = require('../../models').getDatabase();
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended : true }));
 
 // User requests to login with a username and password
-router.post('/', (req, res) => {
+router.post('/login', (req, res) => {
   db.User.findOne({
     where: {
       // TODO: Will want to hash the password and query for that hash
@@ -43,5 +26,5 @@ router.post('/', (req, res) => {
 });
 
 module.exports = (app => {
-  app.use('/login',router);
+  app.use('/api',router);
 });
