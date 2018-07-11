d6bf5b9de1011238732d8fd69c7975752b7edb62
Olivia Sandvold <ollie302@gmail.com>
2018-07-11 14:10:42 -0500

    Updated res.send to JSON objects

--- app/controllers/api/login.js
+++ app/controllers/api/login.js
@@ -1,6 +1,5 @@
 const express = require('express'),
   router = express.Router(),
-  //bcrypt = require('bcrypt');
   argon = require('argon2');
 const db = require('../../models').getDatabase();
 
@@ -14,17 +13,22 @@ router.post('/login', (req, res, next) => {
     }
   }).then (user => {
     // Keep error messages vague to avoid releasing too much information
-    if (!user) return res.status(401).send("Wrong username or password");
+    if (!user)
+      return res.status(401).send({ error: "Wrong username or password"});
 
     argon.verify(user.password, req.body.password).then( match => {
-      if (!match) return res.status(401).send("Wrong username or password");
-      else res.status(200).send("Successful Login");
+      if (!match)
+        return res.status(401).send({ error: "Wrong username or password"});
+      else res.status(200).send({});
     }).catch (err => {
-      return res.status(401).send("There was an internal problem comparing the" +
-      " password using bcrypt");
+      return res.status(401).send({
+        error: "Argon2 could not verify the password"
+      });
     });
   }).catch (err => {
-    return res.status(401).send("There was a problem finding that user in the database" + err.message);
+    return res.status(401).send({
+      error: "There was a problem finding that user in the database" + err.message
+    });
   });
 });
 
