8951eafd7faefdedab30390b059f0db251c10494
Olivia Sandvold <ollie302@gmail.com>
2018-07-11 13:57:11 -0500

    Added salt attribute for users db

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   let User = sequelize.define('User', {
     'username' : DataTypes.STRING,
     'password' : DataTypes.STRING,
+    'salt' : DataTypes.STRING,
     // TODO: Determine how we would like to define access levels
     //'access' : DataTypes.STRING // DataTypes.INTEGER
   }, {
@@ -11,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       associate: (models) => {
         // associations can be defined here
       }
-    }
+    },
   });
   return User;
 }
