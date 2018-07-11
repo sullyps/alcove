c008917eca704f394076b821a5ca02c79f329380
Olivia Sandvold <ollie302@gmail.com>
2018-07-11 13:41:25 -0500

    Implemented Argon2 over Bcrypt

--- app/controllers/api/login.js
+++ app/controllers/api/login.js
@@ -1,32 +1,29 @@
 const express = require('express'),
   router = express.Router(),
-  bcrypt = require('bcrypt');
+  //bcrypt = require('bcrypt');
+  argon = require('argon2');
 const db = require('../../models').getDatabase();
 
 /**
  * Service login requests by matching a username and password to the Database.
  */
-// TODO - why bcrypt??
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   db.User.findOne({
     where: {
       username : req.body.username
     }
-  })
-  .then (user => {
+  }).then (user => {
     // Keep error messages vague to avoid releasing too much information
     if (!user) return res.status(401).send("Wrong username or password");
 
-    bcrypt.compare(req.body.password, user.password).then( match => {
+    argon.verify(user.password, req.body.password).then( match => {
       if (!match) return res.status(401).send("Wrong username or password");
       else res.status(200).send("Successful Login");
-    })
-    .catch (err => {
+    }).catch (err => {
       return res.status(401).send("There was an internal problem comparing the" +
       " password using bcrypt");
     });
-  })
-  .catch (err => {
+  }).catch (err => {
     return res.status(401).send("There was a problem finding that user in the database" + err.message);
   });
 });
