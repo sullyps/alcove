941914dfa78b0e1771d98a6bc107f808122b8858
Olivia Sandvold <ollie302@gmail.com>
2018-07-12 15:57:57 -0500

    Removed auth level TODO, would likely be own table

--- app/models/user.js
+++ app/models/user.js
@@ -4,8 +4,6 @@ module.exports = (sequelize, DataTypes) => {
   let User = sequelize.define('User', {
     'username' : DataTypes.STRING,
     'password' : DataTypes.STRING
-    // TODO: Determine how we would like to define access levels
-    //'access' : DataTypes.STRING // DataTypes.INTEGER
   }, {
     classMethods: {
       associate: (models) => {
