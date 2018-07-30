a02b3a7ce9801ee473b0670628fe8498ca2ea943
Olivia Sandvold <ollie302@gmail.com>
2018-07-30 15:47:30 -0500

    Add comment on attributes that exist, remove unused db

--- app/controllers/api/machine.js
+++ app/controllers/api/machine.js
@@ -1,17 +1,17 @@
 const express = require('express'),
       router = express.Router();
-const db = require('../../models').getDatabase();
 
 const logger = require('../../../lib/config/log4js').getLogger();
 
 router.get('/:id',(req, res, next) => {
   res.render('machine', {
     name : req.params.id,
-    successfulBackups: // integer,
-    totalBackups: // integer,
-    lastBackupDate: // STRING in format 'MM-DD-YYYY HH:MM',
-    timeSinceLastRestart: // STRING in format 'X days, Y hours, Z minutes since last backup',
-    backups: // array of backupEvents
+    // TODO: Write functions to implement the following attributes
+    /*successfulBackups:
+    totalBackups:
+    lastBackupDate:
+    timeSinceLastRestart:
+    backups:*/
   });
 });
 
