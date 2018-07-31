892f6f7f6ad74902e4709d70194cbef21c673f85
Olivia Sandvold <ollie302@gmail.com>
2018-07-31 09:44:14 -0500

    Updated tests to reflect purpose of function

--- tests/numeric.test.js
+++ tests/numeric.test.js
@@ -1,25 +1,24 @@
 const init = require('../lib/config/init.js');
 
-describe('Verifying isNumeric function', () => {
-  test('Integers are numeric', () => {
-    expect(init.__isNumeric(5)).toBe(true);
+// This function takes a string as an input and returns if it is an
+// integer greater than or equal to 0
+describe('Verifying isPositiveInteger function', () => {
+  test('Strings with numbers are positive integers', () => {
+    expect(init.__isPositiveInteger('5')).toBe(true);
   });
-  test('Strings with numbers are numeric', () => {
-    expect(init.__isNumeric('5')).toBe(true);
+  test('Strings with alpha chars are not positive integers', () => {
+    expect(init.__isPositiveInteger('a')).toBe(false);
   });
-  test('Strings with alpha chars are not numeric', () => {
-    expect(init.__isNumeric('a')).toBe(false);
+  test('Hex strings are not positive integers', () => {
+    expect(init.__isPositiveInteger('1F3')).toBe(false);
   });
-  test('Hex strings are not numeric', () => {
-    expect(init.__isNumeric('1F3')).toBe(false);
+  test('Inf is not a positive integer', () => {
+    expect(init.__isPositiveInteger(Infinity)).toBe(false);
   });
-  test('Inf is not numeric', () => {
-    expect(init.__isNumeric(Infinity)).toBe(false);
+  test('Arrays are not positive integers', () => {
+    expect(init.__isPositiveInteger([1,2])).toBe(false);
   });
-  test('Arrays are not numeric', () => {
-    expect(init.__isNumeric([1,2])).toBe(false);
-  });
-  test('Null is not numeric', () => {
-    expect(init.__isNumeric(null)).toBe(false);
+  test('Null is not a positive integer', () => {
+    expect(init.__isPositiveInteger(null)).toBe(false);
   });
 });
