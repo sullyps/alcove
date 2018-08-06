af7132b1621535b6a6b15c02c5232e7eb079d8ef
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-06 12:25:18 -0500

    Initialized config, db, and models in the dashboard controller

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,10 +1,20 @@
 const express = require('express'),
-  router = express.Router();
+      router = express.Router(),
+      system = require('../../lib/system'),
+      models = require('../models'),
+      util = require('../../lib/util');
+
+let config, db, machines;
 
 module.exports = app => {
   app.use('/dashboard', router);
 };
 
 router.get('/', (req, res, next) => {
-  res.render('dashboard', {title: 'Dashboard :: Alcove Backup System'});
+  config = system.getConfig();
+  db = models.getDatabase();
+  machines = system.getMachines();
+  res.render('dashboard', {
+    title: 'Dashboard :: Alcove Backup System'
+  });
 });
