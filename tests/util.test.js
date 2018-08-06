4cfe91caee3765dbd0c715a233b959750eb8534b
Steven Davis <sgdavis@bioneos.com>
2018-08-06 14:25:15 -0500

    Added tests to the empty util module

--- tests/util.test.js
+++ tests/util.test.js
@@ -1,3 +1,10 @@
 const util = require('../lib/util.js');
 
 // Test util.getTimeSinceDate(prevDate, date)
+describe('Testing util.getTimeSinceDate()', () => {
+  test('Same time', () => {
+    var now = new Date();
+    var now2 = new Date(now.getTime());
+    expect(util.getTimeSinceDate(now, now2)).toBe("Backup just occurred");
+  });
+});
