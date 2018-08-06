155886b8ae8b5533addf71d03b3f198730785d6b
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-06 13:37:58 -0500

    Dashboard can find the oldest and newest backup dates

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,5 +1,7 @@
 const express = require('express'),
       router = express.Router(),
+      fs = require('fs'),
+      path = require('path'),
       system = require('../../lib/system'),
       models = require('../models'),
       util = require('../../lib/util');
@@ -14,15 +16,40 @@ router.get('/', (req, res, next) => {
   config = system.getConfig();
   db = models.getDatabase();
   machines = system.getMachines();
+  let sortedBackupDates = getSortedBackupDates();
   res.render('dashboard', {
     title: 'Dashboard :: Alcove Backup System',
     dashboard: {
+      oldestBackupDate: sortedBackupDates[0],
+      newestBackupDate: sortedBackupDates[sortedBackupDates.length - 1],
       lastSummaryEmailDate: getLastSummaryEmailDate()
     }
   });
 });
 
+function getSortedBackupDates() {
+  let backups = [];
+  for (let machineName in machines) {
+    let machinePath = path.join(config.data_dir, machineName);
+    let machineBackups = fs.readdirSync(machinePath).filter(child => {
+      return fs.statSync(path.join(machinePath, child)).isDirectory();
+    });
+    for (let backup of machineBackups) {
+      backups.push(parseISODateString(backup));
+    }
+  }
+  backups.sort((a, b) => {
+    return a.getTime() - b.getTime();
+  });
+  return backups;
+}
+
 function getLastSummaryEmailDate() {
   const date = util.getLastSummaryEmailTime(config.notifications.summary_schedule, new Date());
   return util.getFormattedDate(date).substring(0, 10);
+}
+
+function parseISODateString(dateString) {
+  const dateParts = dateString.replace(/\D+/g, ' ').split(' ');
+  return new Date(Date.UTC(dateParts[0], dateParts[1] - 1, dateParts[2], dateParts[3], dateParts[4], dateParts[5], dateParts[6]));
 }
