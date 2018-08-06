a0f7ab2bec2db2959d21897978b1df1521caab77
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-06 15:12:12 -0500

    Added functions to the dashboard controller to get machine statuses

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -17,12 +17,17 @@ router.get('/', (req, res, next) => {
   db = models.getDatabase();
   machines = system.getMachines();
   let sortedBackupDates = getSortedBackupDates();
+  let machineStatuses = getMachineStatuses();
   res.render('dashboard', {
     title: 'Dashboard :: Alcove Backup System',
     dashboard: {
       oldestBackupDate: util.getFormattedDate(sortedBackupDates[0]).substring(0, 10),
       newestBackupDate: util.getFormattedDate(sortedBackupDates[sortedBackupDates.length - 1]).substring(0, 10),
-      lastSummaryEmailDate: util.getFormattedDate(getLastSummaryEmailDate()).substring(0, 10)
+      lastSummaryEmailDate: util.getFormattedDate(getLastSummaryEmailDate()).substring(0, 10),
+      successfulMachines: machineStatuses[0],
+      partialSuccessMachines: machineStatuses[1],
+      unsuccessfulMachines: machineStatuses[2],
+      idleMachines: machineStatuses[3]
     }
   });
 });
@@ -62,4 +67,76 @@ function getSortedBackupDates()
 function getLastSummaryEmailDate()
 {
   return util.getLastSummaryEmailTime(config.notifications.summary_schedule, new Date());
+}
+
+/**
+ * Gets the statuses of all machines as determined by
+ * the number of scheduled and successful backups.
+ * @returns
+ *   The number of machines of each status
+ *   machineStatuses[0] = Successful machines (all backups succeeded)
+ *   machineStatuses[1] = Partially successful machines (some backups succeeded)
+ *   machineStatuses[2] = Unsuccessful machines (all backups failed)
+ *   machineStatuses[3] = Idle machines (no backups were attempted)
+ */
+function getMachineStatuses()
+{
+  let machineStatuses = [0, 0, 0, 0];
+  for (let machineName in machines)
+  {
+    machineStatuses[getMachineStatus(machineName)]++;
+  }
+  return machineStatuses;
+}
+
+/**
+ * Gets the status of the machine machineName as determined by
+ * the number of scheduled and successful backups.
+ * @param machineName
+ *   The name of the machine to inspect for status
+ * @returns
+ *   The status code of the machine
+ *   0 = Successful machine - All backups succeeded
+ *   1 = Partially successful machine - Some backups succeeded
+ *   2 = Unsuccessful machine - All backups failed
+ *   3 = Idle machine - No backups were attempted
+ */
+function getMachineStatus(machineName)
+{
+  const scheduledBackups = getScheduledBackups(machineName);
+  const successfulBackups = getSuccessfulBackups(machineName);
+  if (scheduledBackups === 0) return 3;
+  else if (successfulBackups === 0) return 2;
+  else if (successfulBackups !== scheduledBackups) return 1;
+  else return 0;
+}
+
+/**
+ * Gets the number of scheduled backups by
+ * the machine machineName.
+ * @param machineName
+ *   The name of the machine to inspect for scheduled backups
+ * @returns
+ *   The number of scheduled backups
+ */
+function getScheduledBackups(machineName)
+{
+  const machine = machines[machineName];
+  let count = 0;
+  util.parseSchedule(machine.schedule).daysSets.forEach(daysSet => {
+    count += daysSet.number;
+  });
+}
+
+/**
+ * Gets the number of successful backups by
+ * the machine machineName.
+ * @param machineName
+ *   The name of the machine to inspect for scheduled backups
+ * @returns
+ *   The number of successful backups
+ */
+function getSuccessfulBackups(machineName)
+{
+  return util.countSubdirectories(path.join(config.data_dir, machineName));
 }
