cf181b36dc0d90f0383eb3173ac84ff2803abb1b
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-07 16:13:19 -0500

    Calculated the last backup date on machine pages

--- app/controllers/machine.js
+++ app/controllers/machine.js
@@ -13,8 +13,8 @@ let config, db;
 
 router.get('/:name',(req, res, next) => {
   // Attempt to grab the machine that is requested
-  let machineObj = system.getMachines()[req.params.name];
-  if (!machineObj)
+  let machine = system.getMachines()[req.params.name];
+  if (!machine)
   {
     logger.warn('Request for unknown machine with name: "' + req.params.name + '"');
     return res.status(404).render('error', {
@@ -25,17 +25,35 @@ router.get('/:name',(req, res, next) => {
     });
   }
 
+  logger.trace(machine);
+
   config = system.getConfig();
   db = models.getDatabase();
 
-  logger.trace(machineObj);
-  res.render('machine', {
-    name: machineObj.name,
-    successfulBackups: util.countSubdirectoriesExclude(path.join(config.data_dir, machineObj.name), [rsync.getInProgressName()]),
-    totalBackups: system.getBuckets(machineObj.schedule, new Date()).length
+  let machineInfo = {
+    name: machine.name,
+    successfulBackups: util.countSubdirectoriesExclude(path.join(config.data_dir, machine.name), [rsync.getInProgressName()]),
+    totalBackups: system.getBuckets(machine.schedule, new Date()).length
+  };
+
+  getLastBackupDate(machine.name)
+  .then(backupEvents => {
+    machineInfo.lastBackupDate = util.getFormattedDate(backupEvents[0].backupTime);
+    res.render('machine', machineInfo);
   });
 });
 
 module.exports = app => {
   app.use('/machine', router);
 };
+
+function getLastBackupDate(machineName)
+{
+  return db.BackupEvent.findAll({
+    where: {
+      machine: machineName,
+      rsyncExitCode: 0
+    },
+    order: [['backupTime', 'DESC']]
+  });
+}
