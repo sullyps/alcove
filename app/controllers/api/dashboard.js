b84244cacf56c99c5dc1b0ce0ced9fe8f041be6e
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-09 15:03:26 -0500

    Added api route for the total size and free space of the entire data directory

---
+++ app/controllers/api/dashboard.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+      router = express.Router(),
+      system = require('../../../lib/system'),
+      util = require('../../../lib/util');
+
+router.get('/size',(req, res, next) => {
+  let config = system.getConfig();
+  return res.json({
+    dirSize: util.getFormattedSize(util.findDirSize(config.data_dir)),
+    freeSpace: util.getFormattedSize(util.findFreeSpace(config.data_dir))
+  });
+});
+
+module.exports = app => {
+  app.use('/api/dashboard', router);
+};
