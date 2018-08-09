566dfb75c8cfcad73c7d2ce2ffde4085000e0a8c
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-08-09 12:10:20 -0500

    Found backup event in machine backup size api route

--- app/controllers/api/machine.js
+++ app/controllers/api/machine.js
@@ -1,24 +1,46 @@
 const express = require('express'),
-      router = express.Router();
+      router = express.Router(),
+      fs = require('fs'),
+      path = require('path'),
+      system = require('../../../lib/system'),
+      models = require('../../models'),
+      rsync = require('../../../lib/rsync'),
+      util = require('../../../lib/util');
 
-const system = require('../../../lib/system');
+let config, db;
+
 const logger = require('../../../lib/config/log4js').getLogger();
 
 router.get('/:name/backup/:backup_id/size',(req, res, next) => {
   // Attempt to grab the machine that is requested
-  let machineObj = system.getMachines()[req.params.name];
-  if (!machineObj) 
+  let machine = system.getMachines()[req.params.name];
+  if (!machine)
   {
     logger.warn('API Request for unknown machine with name: "' + req.params.name + '"');
     return res.status(404).json({ error: 'No machine with name "' + req.params.name + '"' });
   }
 
-  // TODO: Test backup_id in the same way as above
+  logger.trace(machine);
 
-  // Return the value
-  logger.trace(machineObj);
-  //logger.trace(backup);
-  res.json({size: 'TODO'});
+  config = system.getConfig();
+  db = models.getDatabase();
+
+  const id = parseInt(req.params.backup_id, 10);
+  db.BackupEvent.findOne({
+    where: {
+      id: id,
+      machine: machine.name
+    }
+  })
+  .then(backupEvent => {
+    if (!backupEvent)
+    {
+      return res.status(404).json({ error: 'No backup with id "' + id + '" for machine "' + machine.name + '"'});
+    }
+    logger.trace(backupEvent);
+    // TODO: Find backup and return size
+    return res.status(200).json({ size: 'TODO' });
+  });
 });
 
 module.exports = app => {
