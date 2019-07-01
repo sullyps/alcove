80e26e31b1d4384c56816ddbdda81bf6d7f371ee
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2019-07-01 09:40:52 -0500

    Updated Jest and removed vulnerabilities

--- tests/util.test.js
+++ tests/util.test.js
@@ -3,8 +3,8 @@ const util = require('../lib/util.js');
 // Test util.getTimeSinceDate(prevDate, date)
 describe('Testing util.getTimeSinceDate()', () => {
   test('Same time', () => {
-    var now = new Date();
-    var now2 = new Date(now.getTime());
+    const now = new Date();
+    const now2 = new Date(now.getTime());
     expect(util.getTimeSinceDate(now, now2)).toBe("Backup just occurred");
   });
 });
