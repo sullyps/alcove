a627084685ff391972eaa844d3935bc70d2e03a2
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2019-07-05 15:37:45 -0500

    Restrict system to one instance

--- app.js
+++ app.js
@@ -1,100 +1,111 @@
 'use strict';
 const DEVEL = (process.env.NODE_ENV !== "production");
 const CONFIG = (DEVEL ? "." : "") + "/etc/backup/backup.ini";
-// Explicitly warn about non-production modes
-if (DEVEL)
-{
-  console.warn("*** Non-production environment        ***");
-  console.warn("*** Are you sure you want to do this? ***");
-  console.warn("(In a production deployment, you should not ever run 'app.js' directly)\n\n");
-}
 
 // Include 3rd party libraries
 const express = require('express'),
-      fs = require('fs'),
-      http = require('http'),
-      https = require('https'),
-      wrap = require('wordwrapjs');
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    wrap = require('wordwrapjs'),
+    SingleInstance = require('single-instance');
 
 // Include our libraries
 const logging = require('./lib/config/log4js'),
-      configInit = require('./lib/config/init'),
-      models = require('./app/models'),
-      system = require('./lib/system');
+    configInit = require('./lib/config/init'),
+    models = require('./app/models'),
+    system = require('./lib/system');
 
 // Application logger and global Config
 let logger, config, db;
 
-//
-// Config file parsing
-// 
-// NOTE: No logger available yet so minimize output and wrap all messages to
-// fit to an 80 character screen.
-// NOTE2: This is intended to be a system service so the config is hardcoded
-//
-try
-{
-  config = configInit.getConfig(CONFIG);
-}
-catch (error)
-{
-  // Record startup error in the log
-  let msg = '[Config ERROR] ' + error.message;
-  // And output to the console (for immediate feedback to sys-admin)
-  console.error(wrap.wrap(msg, {width: 80, noTrim: true}));
-  // NOTE: This isn't recommended but until unless we replaced the uncaught
-  // exception handler (requires Node >=9.3), it really is the best way. 
-  process.exit(-3);
-}
+// Prevent multiple instances
+const locker = new SingleInstance('alcove-backup-system');
+locker.lock()
+.catch(err => {
+  console.error(err);
+  console.error('You cannot run more than one instance of the Alcove Backup System at the same time.');
+  process.exit(-7);
+})
+.then(() => {
 
-// If in development mode and an rsync key file is specified, give warning
-if (DEVEL && config.rsync.identity)
-{
-  console.warn('**  Warning **');
-  console.warn('*** You are running in a development environment and have ***');
-  console.warn('*** configured an SSH identity in your config file.       ***');
-  console.warn('If you are using a docker container, make sure the file permissions');
-  console.warn('for your keys match the docker user, or your rsync connections');
-  console.warn('will always fail.');
-}
+  // Explicitly warn about non-production modes
+  if (DEVEL)
+  {
+    console.warn("*** Non-production environment        ***");
+    console.warn("*** Are you sure you want to do this? ***");
+    console.warn("(In a production deployment, you should not ever run 'app.js' directly)\n\n");
+  }
 
-//
-// Create the logger as configured
-//
-logging.configure(config);
-logger = logging.getLogger();
-logger.info(config.app.name + ' v' + config.app.version + ' starting up!');
+  //
+  // Config file parsing
+  //
+  // NOTE: No logger available yet so minimize output and wrap all messages to
+  // fit to an 80 character screen.
+  // NOTE2: This is intended to be a system service so the config is hardcoded
+  //
+  try
+  {
+    config = configInit.getConfig(CONFIG);
+  } catch (error)
+  {
+    // Record startup error in the log
+    let msg = '[Config ERROR] ' + error.message;
+    // And output to the console (for immediate feedback to sys-admin)
+    console.error(wrap.wrap(msg, {width: 80, noTrim: true}));
+    // NOTE: This isn't recommended but until unless we replaced the uncaught
+    // exception handler (requires Node >=9.3), it really is the best way.
+    process.exit(-3);
+  }
+
+  // If in development mode and an rsync key file is specified, give warning
+  if (DEVEL && config.rsync.identity)
+  {
+    console.warn('**  Warning **');
+    console.warn('*** You are running in a development environment and have ***');
+    console.warn('*** configured an SSH identity in your config file.       ***');
+    console.warn('If you are using a docker container, make sure the file permissions');
+    console.warn('for your keys match the docker user, or your rsync connections');
+    console.warn('will always fail.');
+  }
+
+  //
+  // Create the logger as configured
+  //
+  logging.configure(config);
+  logger = logging.getLogger();
+  logger.info(config.app.name + ' v' + config.app.version + ' starting up!');
 
 
-//
-// Main Startup chain
-//   1) Load events DB
-//   2) Initialize the Backup system
-//   3) Startup the webapp
-//
-new Promise( (resolve, reject) => {
+  //
+  // Main Startup chain
+  //   1) Load events DB
+  //   2) Initialize the Backup system
+  //   3) Startup the webapp
+  //
+
   // DB
   logger.debug('Loading the Events DB...');
   db = models.init(config);
   logger.trace('  models.init() complete...');
-  resolve(db.sequelize.sync());
+  return db.sequelize.sync();
 })
-.catch( err => {
+.catch(err => {
   logger.error('Error loading the Events DB. Check your configuration and data files: ' + err.message);
   logger.debug(err.stack);
   process.exit(-2);
 })
-.then( () => {
+.then(() => {
   // System
   logger.debug('Starting main process...');
   system.init(config, db);
 })
-.catch( err => {
+.catch(err => {
   logger.error('Error during Backup System startup: ' + err.message);
   logger.debug(err.stack);
   process.exit(-1);
 })
-.then( () => {
+.then(() => {
   // Webapp
   let app = express();
   require('./lib/config/express')(app, config);
