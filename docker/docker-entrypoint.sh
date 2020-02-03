b83f8bb036f70859241862652171e741951def20
Steven Davis <sgdavis@bioneos.com>
2020-02-03 00:26:32 -0600

    Finished reorg for docker-compose

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env sh
-
+cd /opt/alcove
 
 ##
 # On first run of the container, setup the environment
-if [ ! -f /opt/alcove/node_modules/ready ]; then
+if [ ! -f node_modules/ready ]; then
   ##
   # Install / compile modules
-  sudo chmod 777 /opt/alcove/node_modules
+  sudo chmod 777 node_modules
   npm install
-
-  # Symlink SSL cert for visibility
-  ln -s /etc/alcove/ssl/ssl.key node_modules/
-  ln -s /etc/alcove/ssl/ssl.crt node_modules/
 
   # Finish install
   touch node_modules/ready
@@ -19,14 +15,46 @@ fi
   
 ##
 # Ensure permissions work
-echo "Ensuring 'data', 'logs', and 'public/style.css' are writeable..."
+echo "Ensuring 'data', 'logs', 'etc', and 'public/style.css' are writeable..."
 sudo chmod 777 /opt/alcove/data
 sudo chmod 777 /opt/alcove/logs
+sudo chmod 777 /opt/alcove/etc
 sudo chmod 777 /opt/alcove/public/css/style.css
 
-# TODO: check for config, and override 
-#   [secure] section to ensure we always use the self-signed certs from the container
-#   and the data_dir / logs settings to write to the expected bind-mount locations
+# Check for [secure], and override 
+#   [secure] section to ensure we always use the self-signed certs from the
+#   container and the data_dir / logs settings to write to the expected 
+#   bind-mount locations
+key_present=$(grep -c "^\s*key\s*=" config/alcove.ini)
+cert_present=$(grep -c "^\s*cert\s*=" config/alcove.ini)
+data_dir_present=$(grep -c "^\s*data_dir\s*=" config/alcove.ini)
+log_dir_present=$(grep -c "^\s*log_dir\s*=" config/alcove.ini)
+
+if [ -z $key_present ] || [ -z $cert_present ] || [ -z $data_dir_present ] || [ -z $log_dir_present ]; then
+  echo "When running in development mode through docker, please do not configure"
+  echo "any settings for the following values:"
+  echo "  [secure] key"
+  echo "  [secure] cert"
+  echo "  data_dir"
+  echo "  log_dir"
+  echo "These settings will all be automatically applied from the docker scripts."
+  echo "If you truly need to modify this behavior, adjust the script:"
+  echo "  docker/docker-entrypoint.sh"
+  echo "And rebuild your containers..."
+  echo
+  echo "Exiting..."
+  exit 1
+else
+  echo "Copying in machine configurations..."
+  mkdir -p etc/alcove/machines
+  cp -r config/machines/* etc/alcove/machines/
+
+  echo "Appending development values to config"
+  echo "data_dir=./data/\n" > etc/alcove/alcove.ini
+  echo "log_dir=./logs/\n" >> etc/alcove/alcove.ini
+  cat config/alcove.ini >> etc/alcove/alcove.ini
+  echo "[secure]\nkey=/etc/alcove/ssl/ssl.key\ncert=/etc/alcove/ssl/ssl.crt" >> etc/alcove/alcove.ini
+fi
 
 # Startup server
 npm start
